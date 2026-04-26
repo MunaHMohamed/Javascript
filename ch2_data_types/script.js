025f6f851afaaa52f9e61f3e11ae03a8fba2b2ac
Nawal Mohamed <nawalmohamed@Nawals-Air.lan>
2026-04-26 12:17:35 +0100

    I have added ch3_functions

--- ch2_data_types/script.js
+++ ch2_data_types/script.js
@@ -1,8 +1,14 @@
-var firstname = "Nawal"
+/* var firstname = "Nawal"
 var lastname = "Mohamed"
 var age =20
 var isstudent= "true"
 
 console.log(firstname)
 console.log(lastname)
-console.log(isstudent)
+console.log(isstudent) */
+
+var $town8 = "London"
+/* town = "Paris" */
+console.log ($town8)
+var theCityPeopleLikeToExplore = "Dubai"
+console.log(theCityPeopleLikeToExplore)
---
+++ ch3_functions/script.js
@@ -0,0 +1,32 @@
+function showMelondon (){
+console.log("London - 5 Great Things:");
+console.log("1. Rich history and iconic landmarks like the Tower of London");
+console.log("2. Diverse culture and multicultural population");
+console.log("3. World-class museums (many are free)");
+console.log("4. Extensive public transport system");
+console.log("5. Beautiful parks like Hyde Park");
+}
+
+function showMeMilan (){
+console.log("Milan - 5 Great Things:");
+console.log("1. Global fashion capital");
+console.log("2. Stunning architecture like the Duomo di Milano");
+console.log("3. Thriving business and financial hub");
+console.log("4. Incredible Italian cuisine");
+console.log("5. Vibrant art and design scene"); }
+
+function showMeParis (){
+console.log("Paris - 5 Great Things:");
+console.log("1. Iconic landmarks like the Eiffel Tower");
+console.log("2. Renowned art museums like the Louvre");
+console.log("3. Romantic atmosphere and charming streets");
+console.log("4. Exceptional food and café culture");
+console.log("5. Rich history and cultural heritage");}
+
+showMelondon()
+console.log ("*\n")
+showMeMilan()
+console.log ("*\n")
+showMeParis()
+
+
