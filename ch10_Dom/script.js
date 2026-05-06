05d303708695a5b3b1cbbf4b10959ab2f1b69bf2
Muna H Mohamed <muna.atu@gmail.com>
2026-05-06 23:12:21 +0100

    This chapter is complete

--- ch10_Dom/script.js
+++ ch10_Dom/script.js
@@ -1,7 +1,17 @@
+let allWannaFirstBe = document.querySelectorAll("#wanna-first")
+allWannaFirstBe[0].style.color = "purple"
+allWannaFirstBe[0].style.fontSize = "50px"
+allWannaFirstBe[1].style.color = "yellow"
+allWannaFirstBe[1].style.fontSize = "50px"
+
 for (let i =0; i <=500; i++){
     let firstH1 = document.createElement("h1");
-firstH1.textContent = i + " Hello World"
+firstH1.innerHTML= i + " Hello World"
+firstH1.style.color = "violet"
+// let firstP = document.getElementById ("wanna-first")
+// document.body.insertBefore(firstH1, firstP)
+// document.body.removeChild(firstH1)
 document.body.appendChild(firstH1)
-}
 
+}
 
