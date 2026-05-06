0c5cdbfad69a7a7b6456a3228594a7da4994f84d
Muna H Mohamed <muna.atu@gmail.com>
2026-05-07 00:45:23 +0100

    I have done chapter 11  object

---
+++ ch11_object_properties/object.js
@@ -0,0 +1,54 @@
+// let person = {
+//     name: "John Doe",
+//     age: 30,
+//     City: "New York"
+// }
+
+let vehicle = {
+
+    brand:"Toyota",
+    model: "Prious",
+    year: 2020,
+    color: "red",
+    // owner: person,
+
+    stop: function (){
+        console.log("the vehicle has stopped")
+    }
+}
+
+vehicle.stop()
+
+// // console.log (person.name)
+// console.log(person.age)
+
+// console.log(vehicle.owner.name)
+
+// let animal = new Object();
+// animal.type ="dog"
+// animal.bread ="Golden Retriever"
+// console.log (animal)
+
+// let animal = {}
+// animal.type ="dog"
+// animal.bread ="Golden Retriever"
+// console.log(animal)
+
+//  let person = {
+// name: "John Doe",
+//  age: 30,
+//  City: "New York",
+//  saySalaam: function(){
+//     console.log("Asalamu Alaikum")
+//  }
+
+// }
+// person.saySalaam()
+
+// person.name = "Ali Jama"
+// person.age = 50
+// console.log(person.name)
+// console.log(person["name"])
+
+// let {City} = person
+// console.log(City)
